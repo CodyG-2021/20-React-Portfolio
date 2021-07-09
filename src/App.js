6487f5d3b1add9de6585dc076e0366595fb87e90
Cody <gallagher2021@gmail.com>
2021-07-09 12:28:34 -0600

    Got pages added in as grounding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import './App.css';
-import HelloReact from './components/HelloReact';
+// import HelloReact from './components/HelloReact';
+import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
-  return (
-    <div className="portfolio-app">
-			<HelloReact />
-    </div>
-  );
+	return (
+		<div className="portfolio-app">
+			<PortfolioContainer />
+		</div>
+	);
 }
 
 export default App;
