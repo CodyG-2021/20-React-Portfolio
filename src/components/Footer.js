2e144061f6fced2877fdd4f7e897aa3c84a4b592
Cody <gallagher2021@gmail.com>
2021-07-09 20:36:41 -0600

    Added in more css and got the about page up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 export default function Footer() {
-  return (
-<footer className="footer mt-auto py-3">
-	<div className="container text-center">
-		<div >text</div>
-		<br/>
-		<span >© Cody Gallagher 🎲</span>
-	</div>
-</footer>
-  );
+	return (
+		<footer className="footer pt-1">
+			<div className="container text-center">
+				<div>
+					<a className="iconLink" href='https://github.com/CodyG-2021'>
+						<FaGithubSquare size={40} />
+					</a>
+					<a className="iconLink" href='https://github.com/CodyG-2021'>
+						<AiFillLinkedin size={45} />
+					</a>
+				</div>
+				<span >© Cody Gallagher 🎲</span>
+			</div>
+		</footer>
+	);
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 export default function About() {
-  return (
-    <div className='mx-5 mt-3 p-3'>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  );
+	return (
+		<div className='mx-5 mt-3 p-3 aboutMe'>
+			<h1>About Me</h1>
+			<img className="profilePic" src="./prof-pic.jpg" alt="Cody Gallagher" />
+			<p>
+				Born and raised in Colorado and spun around the sun 30 times, I've
+				found passion in fantasy and all that it creates. I have a degree in
+				graphic design and computer graphics from the University of Northern
+				Colorado.
+			</p>
+			<br />
+			<p>
+				I Currently work for PramsPlus as a support specialist for many oil
+				and gas companies that use our production program. With lots of
+				patience I hope to learn a skill I have never fully been able to
+				grasp, coding. With this knowledge I hope to be able to improve
+				myself in my job and one day launch Prams via the web.
+			</p>
+			<br />
+			<p>
+				I enjoy a game of magic whenever I can get it and in my spare time
+				I've picked up tabletop war-gaming. I like to paint models and find
+				it very satisfying to see all the work that slowly progresses on
+				them. I have two cats Dex and Bow who are way too cute! I live with
+				my fiancee and we enjoy eating sushi.
+			</p>
+			<br />
+			<p>
+				Thank you for taking the time to read about me. I hope you enjoy
+				looking at my work and projects soon to come!
+			</p>
+		</div>
+	);
 }
